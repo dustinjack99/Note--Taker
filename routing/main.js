4884a9fac42d3aff5ee631e3f7e5229596b98c9c
unknown <dustin.guy.jackson@gmail.com>
2020-02-25 11:45:28 -0800

    Have all backend routing done - just need to get the index.html to write notes to database

--- routing/main.js
+++ routing/main.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const app = express();
 const PORT = 9001;
-const db = require('../db/db.json')
+const db = require('../db/db.json');
+const path = require('path');
+const fs = require('fs');
 
 
-app.get('/', (req, res) => {
-    res.send('Future Note Taker');
-});
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-app.get('/api/notes', (req, res) => {
-    res.json(db);
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  console.log(newNote);
+//   console.log(res.json(db));
+//   fs.writeFile('../db/db.json', db);
+
 })
 
+app.get('/api/notes', (req, res) => {
+  res.json(db);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
 app.listen(PORT, () => console.log(`app is listening on PORT: ${PORT}`));
+
