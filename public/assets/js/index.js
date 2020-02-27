db4383d808b44e9f43498e9902deef2baa93425e
unknown <dustin.guy.jackson@gmail.com>
2020-02-27 09:56:17 -0800

    NAILED IT. Thanks to Robert, I needed to add app.use(express.static('../public)); to my middleware. Then cleaned up my server code, and we have an app

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -27,7 +27,7 @@ var saveNote = function(note) {
 // A function for deleting a note from the db
 var deleteNote = function(id) {
   return $.ajax({
-    url: "api/notes/" + id,
+    url: "/notes/" + id,
     method: "DELETE"
   });
 };
@@ -50,7 +50,7 @@ var renderActiveNote = function() {
 };
 
 // Get the note data from the inputs, save it to the db and update the view
-var handleNoteSave = function() {
+var handleNoteSave = () => {
   var newNote = {
     title: $noteTitle.val(),
     text: $noteText.val()
--- routing/main.js
+++ routing/main.js
@@ -1,33 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 9001;
-const db = require('../db/db.json');
-const path = require('path');
-const fs = require('fs');
+const PORT = 9002;
+const db = require("../db/db.json");
+const path = require("path");
+const fs = require("fs");
 
-
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("../public"));
 
-app.post('/api/notes', (req, res) => {
+app.post("/api/notes", (req, res) => {
   const newNote = req.body;
   console.log(newNote);
-//   console.log(res.json(db));
-//   fs.writeFile('../db/db.json', db);
-
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(db);
+  db.push(newNote);
+  res.send(db);
+  console.log(db);
+  fs.writeFile("../db/db.json", JSON.stringify(db), err => {
+    if (err) throw err;
+    console.log("saved to database!");
+  });
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get("/api/notes", (req, res) => {
+  res.send(db);
 });
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 app.listen(PORT, () => console.log(`app is listening on PORT: ${PORT}`));
-
